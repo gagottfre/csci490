b867e3b91ceef3014c546dcb26da5fa599f177f3
Gabriel Gottfredsen <gagottfre@cslab07.cs.coastal.edu>
2013-11-05 15:18:28 -0500

    First Commit

---
+++ test.cpp
@@ -0,0 +1,33 @@
+#include <X11/extensions/XTest.h>
+#include <X11/keysym.h>
+#include <X11/Xlib.h>
+#include <iostream>
+
+int main()
+{
+	Display *display;
+	unsigned int keycode;
+	display = XOpenDisplay(NULL);
+
+	//keycode = XKeysymToKeycode(display, XK_t);
+	//XTestFakeKeyEvent(display, keycode, true, 0);
+
+	for(int i = 0; i < 100; i++)
+	{	
+		XTestFakeRelativeMotionEvent(display, 1, 1, 10);
+	}
+	for(int i = 0; i < 100; i++)
+	{	
+		XTestFakeRelativeMotionEvent(display, 1, -1, 10);
+	}
+	for(int i = 0; i < 100; i++)
+	{	
+		XTestFakeRelativeMotionEvent(display, -1, -1, 10);
+	}
+	for(int i = 0; i < 100; i++)
+	{	
+		XTestFakeRelativeMotionEvent(display, -1, 1, 10);
+	}
+
+	XFlush(display);
+}
